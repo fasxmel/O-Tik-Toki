d45e129d8462518351dc084494e10188e214ba5b
Hristoz Ognyanov <hristozognyanov1984@gmail.com>
2024-10-28 15:20:42 +0100

    Initial Project and dependency

---
+++ o-tik-toki/next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  webpack: (config, { isServer }) => {
+    //Add a rule to handle the canvas.node module
+    config.module?.rules?.push({
+      test: /\.node$/,
+      use: "raw-loader",
+    })
+    // Exclude canvas from being processed by Next.js in the browser
+    if (!isServer) config.externals.push('canvas');
+    return config
+    
+  }
+};
+
+export default nextConfig;
