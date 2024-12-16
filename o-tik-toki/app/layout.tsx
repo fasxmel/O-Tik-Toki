c14f9acfdb1af8b837624f844ba4d141749787f0
Hristoz Ognyanov <hristozognyanov1984@gmail.com>
2024-12-16 14:12:34 +0100

    fix small bug

--- o-tik-toki/app/layout.tsx
+++ o-tik-toki/app/layout.tsx
@@ -17,8 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-     <AuthOverlay />
+     
       <body>
+       <AuthOverlay />
         {children}
       </body>
     </html>
